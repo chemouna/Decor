1cde55cf1671d4b3a4e4dc783afffacfbd915531
Madis Pink <madis.pink@gmail.com>
2014-03-08 17:06:02 +0200

    Prep for maven publishing

--- library/src/androidTest/java/com/madisp/pretty/PrettyTest.java
+++
@@ -1,9 +0,0 @@
-package com.madisp.pretty;
-
-import android.test.AndroidTestCase;
-
-public class PrettyTest extends AndroidTestCase {
-	public void testSanity() throws Exception {
-		assertTrue(true);
-	}
-}
