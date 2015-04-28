58a9a1dba9ca5b3ed45b13de6dcea5afe04ce0d7
Mouna Cheikhna <cheikhnamouna@gmail.com>
2015-04-28 13:51:30 -0300

    add test case for applying decorator only on correct widget

--- decor/src/test/java/com/mounacheikhna/decor/test/DecorFactoryTest.java
+++ decor/src/test/java/com/mounacheikhna/decor/test/DecorFactoryTest.java
@@ -4,8 +4,12 @@ import android.content.Context;
 import android.test.mock.MockContext;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.mounacheikhna.decor.AttrsDecorator;
 import com.mounacheikhna.decor.DecorFactory;
+import com.mounacheikhna.decor.DecorValue;
 import com.mounacheikhna.decor.Decorator;
 
 import org.junit.Before;
@@ -16,7 +20,9 @@ import org.mockito.Mockito;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.MockitoAnnotations.initMocks;
@@ -27,9 +33,12 @@ import static org.mockito.MockitoAnnotations.initMocks;
 public class DecorFactoryTest {
 
     @Mock Decorator decorator;
+    @Mock AttrsDecorator<TextView> textViewDecorator;
+
     Context context;
     View view;
-    View parent;
+    TextView textView;
+    @Mock ViewGroup parent;
     @Mock AttributeSet attributeSet;
 
     private DecorFactory decorFactory;
@@ -40,9 +49,13 @@ public class DecorFactoryTest {
         initMocks(this);
         context = new MockContext();
         view  = new View(context);
-        parent = new View(context);
+        parent.addView(view); //this is maybe just useless since parent is a mock
+        textView = new TextView(context);
+        parent.addView(textView); //maybe useless ?
+
         decorators = new ArrayList<>();
         decorFactory = new DecorFactory(decorators);
+
     }
 
     @Test
@@ -53,6 +66,14 @@ public class DecorFactoryTest {
         String name = "android.view.View";
         decorFactory.onViewCreated(view, name, parent, context, attributeSet);
         verify(decorator, times(1)).apply(view, parent, name, context, attributeSet);
+    }
 
+    @Test
+    public void applyDecoratorIsAplliedOnlyOnCorrectWidget() throws Exception {
+        decorators.add(textViewDecorator);
+        String name = "android.widget.TextView";
+        decorFactory.onViewCreated(view, name, parent, context, attributeSet);
+        //verify(textViewDecorator, never()).apply(view, any(DecorValue.class)); //pb here is that apply is
+                                                // protected (we can't make it public, it changes a lot)
     }
 }
